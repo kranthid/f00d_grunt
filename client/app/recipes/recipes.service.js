a9a53d690109d34a4a829c828a1ddbbe78998097
Sai kumar <sappanab@cisco.com>
2015-12-28 20:42:00 +0530

    Category directive added for menu items creation

--- client/app/recipes/recipes.service.js
+++ client/app/recipes/recipes.service.js
@@ -4,10 +4,8 @@ angular.module('myNewProjectApp')
   .factory('recipes',['$http', function ($http) {
     // Public API here
     return {
-      uploadFile: function (fileData) {
-      	console.log("++++++++++++",fileData)
-        var fd = new FormData();
-         fd.append("uploadedFile", fileData);
+      createMenuItem: function (menuData) {
+      	console.log("++++++++++++",menuData)
         return $http.post('/cloud/upload',fd.fileObject,{
             transformRequest: angular.identity,
             headers: {'Content-Type': undefined}
