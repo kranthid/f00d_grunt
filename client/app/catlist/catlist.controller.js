2cde68dea985e8445d292761850592701c8d04ce
Sai kumar <sappanab@cisco.com>
2015-12-28 11:57:39 +0530

    Fixed categories initial loading issue

--- client/app/catlist/catlist.controller.js
+++ client/app/catlist/catlist.controller.js
@@ -4,11 +4,11 @@ angular.module('myNewProjectApp')
   .controller('CatlistCtrl',['$scope','catlist','$state',function ($scope,catlist,$state) {
 		$scope.title = 'List Of Categories';
 		$scope.showCatlist = function(){
-			return catlist.getAllCategories().then(function(returnData){
+/*			return catlist.getAllCategories().then(function(returnData){
 				$scope.catlist = returnData.data;
 			},function(error){
 				$scope.catlist = [];
-			});
+			});*/
 		}
 
 		$scope.updateCategory = function(updateData){
