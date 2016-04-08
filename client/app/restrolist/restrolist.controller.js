ac5013e82aa80131fd057d621b9f3dffe53282a2
Sai kumar <sappanab@cisco.com>
2016-04-08 22:55:14 +0530

    Home page placeholder done

--- client/app/restrolist/restrolist.controller.js
+++ client/app/restrolist/restrolist.controller.js
@@ -9,7 +9,7 @@ angular.module('myNewProjectApp')
     }
     $scope.removeMenuItem = function(getMenuDetails){
     		console.log("Item to be deleted >>",getMenuDetails);
-            var deleteDecision = prompt("Are you sure then type DELETE");
+            var deleteDecision = prompt("Are you sure...Then type DELETE");
             if(deleteDecision == "DELETE"){
                 restroser.deleteHotelItem(getMenuDetails).then(function(res){
                     $scope.deletionResult = "Hotel was deleted successfully";
@@ -17,7 +17,7 @@ angular.module('myNewProjectApp')
                 },function(error){
                     $scope.deletionResult = "Some error occured"
                 })
-                
+
             }else{
                 alert("Your Menu Item is not yet deleted")
 
